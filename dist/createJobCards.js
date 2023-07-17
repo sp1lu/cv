585855c203b7f88c8d40d134455f04d5f68327d8
sp1lu <davide.rivolta@yahoo.it>
2023-07-17 10:18:10 +0200

    Base Bootstrap layout

--- dist/createJobCards.js
+++ dist/createJobCards.js
@@ -5,7 +5,7 @@ const jobExperiences = document.querySelector('.job-experiences');
 export const createJobsCards = function () {
     for (const item of jobs) {
         const card = document.createElement('div');
-        card.classList.add('card');
+        card.classList.add('card','border', 'border-0', 'bg-dark', 'text-light', 'mb-1', 'p-3');
 
         const years = document.createElement('div');
         years.classList.add('years');
@@ -16,7 +16,7 @@ export const createJobsCards = function () {
         work.classList.add('job-experience');
 
         const roleAndAgency = document.createElement('div');
-        roleAndAgency.classList.add('role-agency');
+        roleAndAgency.classList.add('role-agency', 'd-flex', 'align-middle');
         work.append(roleAndAgency);
 
         const role = document.createElement('h4');
@@ -25,7 +25,7 @@ export const createJobsCards = function () {
         roleAndAgency.append(role);
 
         const agency = document.createElement('a');
-        agency.classList.add('agency');
+        agency.classList.add('agency', 'ms-3', 'pt-1', 'link-underline', 'link-underline-opacity-0');
         agency.textContent = item.agency;
         agency.href = item.link;
         agency.setAttribute('target', '_blank');
